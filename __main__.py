91f09ebe648d519dd38c6ad042e94c30208607db
Rainwater <collinziolko@hotmail.com>
2026-02-19 07:32:23 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -6,36 +6,29 @@ import os
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip as mpy
 from tqdm import tqdm
 
-def rotate_4d(vector, theta, plane='xw'):
-  """
-  Rotates a 4D vector (x, y, z, w) by angle theta in specified plane.
-  """
-  c, s = np.cos(theta), np.sin(theta)
-  
-  # Initialize 4x4 Identity Matrix
-  R = np.eye(4)
+def rotate(vector, theta, plane):
+  # Convert point to a numpy column vector [x, y, z, w]
+  v = np.array(vector)
     
-  # Define rotation matrix for a plane (e.g., xw plane)
-  if plane == 'xw':
-    R[0, 0] = c; R[0, 3] = -s
-    R[3, 0] = s; R[3, 3] = c
-  elif plane == 'xy':
-    R[0, 0] = c; R[0, 1] = -s
-    R[1, 0] = s; R[1, 1] = c
-  elif plane == 'zw':
-    R[2, 2] = c; R[2, 3] = -s
-    R[3, 2] = s; R[3, 3] = c
+  # Initialize a 4x4 Identity Matrix
+  m = np.eye(4)
+  c, s = np.cos(theta), np.sin(theta)
+
+  # Apply the rotation to the specific plane
+  # In 4D, there are 6 primary planes: xy, xz, xw, yz, yw, zw
+  if plane == 'xy':
+      m[0, 0], m[0, 1], m[1, 0], m[1, 1] = c, -s, s, c
   elif plane == 'yz':
-    R[1, 1] = c; R[1, 3] = -s
-    R[2, 1] = s; R[2, 3] = c
+      m[1, 1], m[1, 2], m[2, 1], m[2, 2] = c, -s, s, c
   elif plane == 'xz':
-    R[0, 0] = c; R[0, 2] = -s
-    R[2, 0] = s; R[2, 2] = c
+      m[0, 0], m[0, 2], m[2, 0], m[2, 2] = c, s, -s, c
+  elif plane == 'xw':
+      m[0, 0], m[0, 3], m[3, 0], m[3, 3] = c, -s, s, c
   elif plane == 'yw':
-    R[1, 1] = c; R[1, 3] = -s
-    R[3, 1] = s; R[3, 3] = c
-  
-  return np.dot(R, vector)
+      m[1, 1], m[1, 3], m[3, 1], m[3, 3] = c, -s, s, c
+  elif plane == 'zw':
+      m[2, 2], m[2, 3], m[3, 2], m[3, 3] = c, -s, s, c
+  return m @ v
 
 
 fig = plt.figure()
@@ -50,8 +43,9 @@ cw = 0
 x = [-1,-1,-1,-1, 1, 1, 1, 1,-1,-1,-1,-1, 1, 1, 1, 1]
 y = [-1,-1, 1, 1,-1,-1, 1, 1,-1,-1, 1, 1,-1,-1, 1, 1]
 z = [-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1, 1]
+#w = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 w = [-1,-1,-1,-1,-1,-1,-1,-1, 1, 1, 1, 1, 1, 1, 1, 1]
-
+origin = [0,0,0,0]
 s = [ 0, 0, 0, 7, 7, 7, 1, 5, 4, 6, 2, 3, 0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8,15,15,15, 9,13,12,14,10,11]
 e = [ 1, 2, 4, 6, 5, 3, 5, 4, 6, 2, 3, 1, 8, 9,10,11,12,13,14,15, 9,10,12,14,13,11,13,12,14,10,11, 9]
 
@@ -81,22 +75,33 @@ def update_lines(x, y, z):
     line_ez.append(z[n])
   return line_sx, line_sy, line_sz, line_ex, line_ey, line_ez
 
-def update_tick():
+def update_tick(vector,o_vector,fov=90,distance=2,azim=0,elev=0):
   plt.cla()
-  ax.view_init(azim=0)
+  if fov == 180:
+    ax.set_proj_type('ortho')
+  else:
+    fl = 1/np.tan(fov/2)
+    ax.set_proj_type('persp', focal_length=fl)
+  ax.set_xlim(-1.5, 1.5)
+  ax.set_ylim(-1.5, 1.5)
+  ax.set_zlim(-1.5, 1.5)
+  ax.view_init(azim=azim, elev=elev)
   ax.set_axis_off()
   ax.set_facecolor('#000000')
-  xp = []
-  yp = []
-  zp = []
-  for n in range(len(w)):
-    xp.append(x[n])
-    yp.append(y[n])
-    zp.append(z[n])
+  xp = vector[0].copy()
+  yp = vector[1].copy()
+  zp = vector[2].copy()
+  wp = vector[3].copy()
+  xo, yo, zo, wo = o_vector
+  for n in range(len(xp)):
+    w_factor = 1 / (distance - (wp[n]+ wo))
+    xp[n] = (xp[n] + xo) * w_factor
+    yp[n] = (yp[n] + yo) * w_factor
+    zp[n] = (zp[n] + zo) * w_factor
   line_sx, line_sy, line_sz, line_ex, line_ey, line_ez = update_lines(xp, yp, zp)
   for i in range(len(line_sx)):
     ax.plot([line_sx[i], line_ex[i]], [line_sy[i],line_ey[i]],zs=[line_sz[i],line_ez[i]], color='#FFFFFF')
-  ax.scatter(x,y,z, color='#FFFFFF')
+  ax.scatter(xp,yp,zp, color='#FFFFFF')
 
 def mkdir(folder):
   try:
@@ -109,26 +114,45 @@ def mkdir(folder):
 
 def build_video(image_folder, output_path, frames, fps):
   image_files = [] 
-  for img_number in range(1,frames): 
+  for img_number in range(0,frames): 
     image_files.append(os.path.join(image_folder, f'{img_number}.png'))
   clip = mpy(image_files, fps=fps)
   clip.write_videofile(output_path)
+
+def keyframe(folder, sframe, eframe,vectors,origin,rotate_plane='xy',rotate_theta=0,transform=[0,0,0,0],scale=[1,1,1,1]):
+  framel = eframe-sframe
+  xv, yv, zv, wv = list(zip(*vectors))
+  xv = list(xv)
+  yv = list(yv)
+  zv = list(zv)
+  wv = list(wv)
+  
+  xs, ys, zs, ws = scale
+  
+  xo, yo, zo, wo = origin
+
+  xt, yt, zt, wt = transform
+  
+  for n in tqdm(range(int(framel))):
+    for o in range(len(vectors)):
+      #xv[o], yv[o], zv[o], wv[o] = xv[o]*((1-(n/framel))+xs*(n/framel)), yv[o]*((1-(n/framel))+ys*(n/framel)), zv[o]*((1-(n/framel))+zs*(n/framel)), wv[o]*((1-(n/framel))+ws*(n/framel))
+      #xo, yo, zo, wo = xo+xt, yo+yt, zo+zt, wo+wt
+      xv[o], yv[o], zv[o], wv[o] = rotate([xv[o], yv[o], zv[o], wv[o]], (np.pi/180)*rotate_theta, rotate_plane)
+    update_tick([xv,yv,zv,wv],[xo, yo, zo, wo], fov=90,elev=30)
+    plt.savefig(os.path.join(folder, os.path.join('tmp',f'{int(sframe+n)}.png')))
+  return list(zip(xv,yv,zv,wv))
 
 def animation(folder, frames, fps):
   mkdir(folder)
   mkdir(os.path.join(folder,'tmp'))
   mkdir(os.path.join(folder,'build'))
-  for n in tqdm(range(frames)):
-    #ax.view_init(azim=n)
-    r = (np.pi/180)s #convert degrees to radians
-    for o in range(len(x)):
-      x[o], y[o], z[o], w[o] = rotate_4d([x[o], y[o], z[o], w[o]], r, 'xw')
-    update_tick()
-    plt.savefig(os.path.join(folder, os.path.join('tmp',f'{n}.png')))
-  #shutil.make_archive('frames', 'zip', root_dir=folder)
+  p = list(zip(x,y,z,w))
+
+  keyframe(folder, 0,90,p,origin,rotate_plane='xy',rotate_theta=1)
+  keyframe(folder, 90,180,p,origin,rotate_plane='zw',rotate_theta=1)
+  keyframe(folder, 180,270,p,origin,rotate_plane='xw',rotate_theta=1)
+  keyframe(folder, 270,360,p,origin,rotate_plane='yw',rotate_theta=1)
+  
   build_video(os.path.join(folder, 'tmp'), os.path.join(folder, f'build/0-{frames}.mp4'), frames, fps)
   
-
-  
 animation("ani", 360, 30)
-
